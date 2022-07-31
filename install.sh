fd9c6fae1ad88aab80f1bcd1caf5bad085b3cc00
lthero-big <70460095+lthero-big@users.noreply.github.com>
2022-07-31 19:59:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ conf_dir="/home/autoSign"
 project_dir="/home/autoSign/TodayStudyAutoSign"
 ID=`cat /etc/issue|awk '{gsub(/^\s+|\s+$/, "");print $1}'`
 VERSION_ID=`cat /etc/issue|awk '{gsub(/^\s+|\s+$/, "");print $2}'`
+timeZone=`date -R |  awk -F '[ ]'  '{print $6}'`
+reDownFlag=1
 #fonts color
 Green="\033[32m"
 Red="\033[31m"
@@ -31,7 +33,6 @@ checkSystem() {
     elif [[ "${ID}" == "Debian" && ${VERSION_ID} -ge 8 ]]; then
         echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font}"
         INS="apt"
-        ## 添加 Nginx apt源
     elif [[ "${ID}" == "Ubuntu" && $(echo "${VERSION_ID}" | cut -d '.' -f1) -ge 16 ]]; then
         echo -e "${OK} ${GreenBG} 当前系统为 Ubuntu ${VERSION_ID} ${UBUNTU_CODENAME} ${Font}"
         INS="apt"
@@ -46,8 +47,9 @@ checkSystem() {
 }
 
 chronyInstall() {
+if [ ${timeZone} != "+0800" ]; then
     ${INS} -y install chrony
-    judge "安装 chrony 时间同步服务 "
+    judge "正在安装 chrony 时间同步服务 "
 
     timedatectl set-ntp true
 
@@ -79,39 +81,40 @@ chronyInstall() {
         exit 2
         ;;
     esac
+fi
+    judge "时间同步完成"
 }
 
 dirCreate() {
-    rm -rf ${conf_dir}
-    mkdir -p ${conf_dir}
-    cd ${conf_dir} || exit
-    git clone https://github.com/lthero-big/TodayStudyAutoSign.git
-    judge "项目已经下载"
+    if [ -d ${conf_dir} ];then
+        echo "项目已经存在，是否重新下载？重新下载后原配置会被删除"
+        echo -e "${Green}1.${Font}  是 "
+        echo -e "${Green}2.${Font}  否 "
+        read -rp "请输入数字：" isRedown
+        case $isRedown in
+        1)
+            reDownFlag=1
+            ;;
+        *)
+            echo "项目未重新下载"
+            reDownFlag=0
+            ;;
+        esac  
+    fi
+    if [ $reDownFlag -eq 1 ];then
+        rm -rf ${conf_dir}
+        mkdir -p ${conf_dir}
+        cd ${conf_dir} || exit
+        git clone https://github.com/lthero-big/TodayStudyAutoSign.git
+        judge "项目下载完成"
+    fi
 }
 evenmentCreate(){
     ${INS} -y install python3-pip
-    judge "python3-pip安装完成"
+    judge "python3-pip 安装完成"
     cd ${project_dir}
     /usr/bin/pip3 install -r /home/autoSign/TodayStudyAutoSign/requirements.txt
-    judge "环境已经安装完毕"
-}
-cronCreate() {
-    if [[ "${ID}" == "Centos" ]]; then
-        ${INS} -y install crontabs
-    else
-        ${INS} -y install cron
-    fi
-    judge "安装 crontab"
-
-    if [[ "${ID}" == "Centos" ]]; then
-        touch /var/spool/cron/root && chmod 600 /var/spool/cron/root
-        systemctl start crond && systemctl enable crond
-    else
-        touch /var/spool/cron/crontabs/root && chmod 600 /var/spool/cron/crontabs/root
-        systemctl start cron && systemctl enable cron
-
-    fi
-    judge "crontab 自启动配置 "
+    judge "相关环境 安装完成"
 }
 autoShellAdd() {
     touch ${conf_dir}/autoSign.sh
@@ -125,41 +128,89 @@ echo "--------------------------------------------------------------------------
 EOF
     chmod +x ${conf_dir}/autoSign.sh
     touch ${conf_dir}/autoSignLog
-    judge "shell 脚本已添加"
+    judge "shell 执行脚本添加完成"
 }
+cronCreate() {
+    if [[ "${ID}" == "Centos" ]]; then
+        ${INS} -y install crontabs
+    else
+        ${INS} -y install cron
+    fi
 
-cronUpdate() {
+    if [[ "${ID}" == "Centos" ]]; then
+        touch /var/spool/cron/root && chmod 600 /var/spool/cron/root
+        systemctl start crond && systemctl enable crond
+    else
+        touch /var/spool/cron/crontabs/root && chmod 600 /var/spool/cron/crontabs/root
+        systemctl start cron && systemctl enable cron
+
+    fi
+    judge "cron 安装完成"
+
     if [[ $(crontab -l | grep -c "autoSign.sh") -lt 1 ]]; then
-      if [[ "${ID}" == "Centos" ]]; then
+      if read -t 10 -p "输入每天几点打卡:" Hour
+	then
+    	echo "每天$Hour点整打卡"
+      else
+    	echo "\n默认10点打卡。"
+      fi
+	if [[ "${ID}" == "Centos" ]]; then
           #        sed -i "/acme.sh/c 0 3 * * 0 \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
           #        &> /dev/null" /var/spool/cron/root
-	  sed -i "1i 0 10 * * * ${conf_dir}/autoSign.sh >> ${conf_dir}/autoSignLog 2>&1" /var/spool/cron/root
+        sed -i "1,3d" /var/spool/cron/root
+	    sed -i "1i 0 ${Hour} * * * ${conf_dir}/autoSign.sh >> ${conf_dir}/autoSignLog 2>&1" /var/spool/cron/root
       else
           #        sed -i "/acme.sh/c 0 3 * * 0 \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
           #        &> /dev/null" /var/spool/cron/crontabs/root
-	  sed -i "1i 0 10 * * * ${conf_dir}/autoSign.sh >> ${conf_dir}/autoSignLog 2>&1" /var/spool/cron/crontabs/root
+        sed -i "1,3d" /var/spool/cron/crontabs/root
+	    sed -i "1i 0 ${Hour} * * * ${conf_dir}/autoSign.sh >> ${conf_dir}/autoSignLog 2>&1" /var/spool/cron/crontabs/root
       fi
     fi
     judge "cron 计划任务更新"
 }
+cronUpdate() {
+    if [[ $(crontab -l | grep -c "autoSign.sh") -eq 1 ]]; then
+        if read -t 10 -p "输入每天几点打卡:" Hour;then
+            echo "每天$Hour点整打卡"
+        else
+            echo "超时未输入，默认10点打卡。"
+	    Hour=10
+        fi
+
+	    if [[ "${ID}" == "Centos" ]]; then
+            sed -i '1d' /var/spool/cron/root
+            sed -i "1i 0 ${Hour} * * * ${conf_dir}/autoSign.sh >> ${conf_dir}/autoSignLog 2>&1" /var/spool/cron/root
+        else
+            sed -i '1d' /var/spool/cron/crontabs/root
+	        sed -i "1i 0 ${Hour} * * * ${conf_dir}/autoSign.sh >> ${conf_dir}/autoSignLog 2>&1" /var/spool/cron/crontabs/root
+      fi
+      judge "cron 计划任务更新"
+    fi
+}
+infoUpdate(){
+    echo "信息配置文件位置:${project_dir}/config.yml"
+    echo "具体如何配置请查看:${project_dir}/config_demo.yml"
+}
 
 installMain(){
-
-checkSystem
-chronyInstall
-dirCreate
-evenmentCreate
-autoShellAdd
-cronCreate
-cronUpdate
+    checkSystem
+    chronyInstall
+    dirCreate
+    if [ $reDownFlag -eq 1 ];then 
+        evenmentCreate
+        autoShellAdd
+        cronCreate
+    fi
+    echo -e "${Green}—————————————— 脚本运行完成 ——————————————${Font}"
 }
 
 main() {
-    echo -e "\t autoSign脚本 ${Red} ${Font}"
-    echo -e "—————————————— 使用手册 ——————————————"""
-    echo -e "${Green}0.${Font}  安装 脚本"
-    echo -e "${Green}1.${Font}  修改配置 "
-    echo -e "${Green}2.${Font} 退出 \n"
+    
+    echo -e "—————————————— autoSign脚本 ——————————————"""
+    echo -e "${Green}0.${Font}  安装脚本"
+    echo -e "${Green}1.${Font}  修改打卡时间 "
+    echo -e "${Green}2.${Font}  修改登录信息 "
+    echo -e "${Green}3.${Font}  退出 \n"
 
     read -rp "请输入数字：" menu_num
     case $menu_num in
@@ -167,9 +218,12 @@ main() {
         installMain
         ;;
     1)
-        changeConfig
+        cronUpdate
         ;;
     2)
+        infoUpdate
+        ;;
+    3)
         exit 0
         ;;
     *)
